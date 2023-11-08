cc2c480ce6363a5fd071629ea19589be67f3e96e
DanilaDool <Levhenko-2626@mail.ru>
2023-11-09 00:30:53 +0300

    add_model_friendship_and_controller_friendships

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'users#index'
   resources :users, only: [:edit, :update, :index]
   resource :home, only: [:index]
   # resources :friendships, only: [:create, :update, :destroy, :show]
